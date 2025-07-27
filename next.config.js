07b40a70cbd6e6808589a2aadf4e75ffcfb2f8d7
1-Maryam <bsef22m535@pucit.edu.pk>
2025-07-27 14:43:30 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
-  }
-}
-webpack: (config) => {
-  config.module.rules.push({
-    test: /\.test\.js$/,
-    use: 'ignore-loader',
-  });
-  return config;
-}
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.test\.js$/,
+      use: 'ignore-loader',
+    });
 
-module.exports = nextConfig
+    // Optional: Also ignore test folder inside pdf-parse completely
+    config.externals.push('pdf-parse/test/*');
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
